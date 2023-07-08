dfe5428bba458d6575cfc3dc6d151a309021af04
bruno <bruno.cabral.silva2018@gmail.com>
2023-07-07 22:41:16 -0300

    Criação de Footer Simples

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -7,38 +7,42 @@ import ShoppingCart from './pages/ShoppingCart';
 import Profile from './pages/Profile';
 import Requests from './pages/Requests';
 import NotFound from './pages/NotFound';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={<Navigate replace to ="/home" />}
-      />
-      <Route
-        path="/home"
-        element={<Home />}
-      />
-      <Route
-        path="/favoritos"
-        element={<Favorites />}
-      />
-      <Route
-        path="/carrinho-de-compras"
-        element={<ShoppingCart />}
-      />
-      <Route
-        path="/meus-pedidos"
-        element={<Requests />}
-      />
-      <Route
-        path="/perfil"
-        element={<Profile />}
-      />
-      <Route
-        path="*"
-        element={<NotFound />}
-      />
-    </Routes>
+    <main>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate replace to ="/home" />}
+        />
+        <Route
+          path="/home"
+          element={<Home />}
+        />
+        <Route
+          path="/favoritos"
+          element={<Favorites />}
+        />
+        <Route
+          path="/carrinho-de-compras"
+          element={<ShoppingCart />}
+        />
+        <Route
+          path="/meus-pedidos"
+          element={<Requests />}
+        />
+        <Route
+          path="/perfil"
+          element={<Profile />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+      <Footer />
+    </main>
   );
 }
---
+++ front-end/src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Footer() {
+  return(
+    <footer className="bg-black flex items-center justify-center flex-col text-white text-sm md:flex-row md:justify-between px-5 py-2">
+      <div>
+        <img
+          src={require('../images/white-elephant.png')}
+          alt="Logo do Safari"
+          className="w-12"
+        />
+      </div>
+      <div className="flex flex-col md:flex-row items-center md:items-end justify-center text-center">
+        <span>© 2023 Copyright - Bruno Gabryell Cabral</span>
+        <span className="hidden md:flex px-2">|</span>
+        <span>bruno.cabral.silva2018@gmail.com</span>
+        <span className="hidden md:flex px-2">|</span>
+        <span>+ 55 (83) 9 9836-4408</span>
+      </div>
+    </footer>
+  );
+}
+  
